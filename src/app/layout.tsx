67fe14fcd7c7ebe64e22c67858e1c1474cf2ea84
F e l p <lipjb@hotmail.com>
2024-04-05 09:48:17 -0300

    fix atts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 /** @format */
-'use client'
+"use client";
 
 // import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -15,13 +15,17 @@ const inter = Inter({ subsets: ["latin"] });
 // };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   const queryClient = new QueryClient();
   return (
     <html lang="en">
+      <head>
+        <title>Weather Forecast</title>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <QueryClientProvider client={queryClient}>
         <body className={inter.className}>{children}</body>
       </QueryClientProvider>
