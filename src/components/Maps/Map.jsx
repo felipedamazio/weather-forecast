efc000d50e9d734ae5433915375e96234139570a
F e l p <lipjb@hotmail.com>
2024-04-19 00:16:08 -0300

    fix: miliseconds marker change

--- src/components/Maps/Map.jsx
+++ src/components/Maps/Map.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from "react";
 import * as maptilersdk from "@maptiler/sdk";
 import "@maptiler/sdk/dist/maptiler-sdk.css";
 import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import { FaTemperatureHigh, FaTemperatureLow } from "react-icons/fa";
 
 export function Map({ data }) {
   const API_MAP_KEY = process.env.NEXT_PUBLIC_API_MAPTILER_KEY;
@@ -16,16 +17,14 @@ export function Map({ data }) {
   maptilersdk.config.apiKey = API_MAP_KEY;
 
   useEffect(() => {
-    if (!map.current) {
-      map.current = new maptilersdk.Map({
-        container: mapContainer.current,
-        style: maptilersdk.MapStyle.STREETS,
-        center: [position.lng, position.lat],
-        zoom: zoom,
-      });
-    } else {
-      map.current.panTo([position.lng, position.lat]);
-    }
+    !map.current
+      ? (map.current = new maptilersdk.Map({
+          container: mapContainer.current,
+          style: maptilersdk.MapStyle.STREETS,
+          center: [position.lng, position.lat],
+          zoom: zoom,
+        }))
+      : map.current.panTo([position.lng, position.lat],{duration: 5000});
 
     // Add a new marker
 
@@ -35,10 +34,13 @@ export function Map({ data }) {
       .setLngLat([position.lng, position.lat])
       .setPopup(
         new maptilersdk.Popup().setHTML(
-          `<span>${data?.city.name}-${data?.city.country} </span>  
-          <span class='degree'>${convertKelvinToCelsius(
-            firstData?.main.temp ?? 0
-          )}°c</span>`
+          `<span>
+        ${data?.city.name}-${data?.city.country}
+      </span>
+      <span class="degree">
+        ${convertKelvinToCelsius(firstData?.main.temp ?? 0)}°c
+      </span>
+      `
         )
       )
       .addTo(map.current);
